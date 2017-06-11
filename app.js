a781931221bd4f0af3e383c58969758f67d2d79f
Christian Arredondo <christianarredondo@utexas.edu>
2017-06-11 14:35:21 -0500

    Move all files to new repo

---
+++ app.js
@@ -0,0 +1,69 @@
+
+// Bind Login button to modal
+$(document).on('click','#loginButton', function(){
+      $('#loginModal').modal('show');
+  });
+
+// Set initial email and password test values:
+var emailTest = false;
+var passwordTest = false;
+console.log('email test = ' + emailTest);
+console.log('password test = ' + passwordTest);
+
+$('form').submit(function() {
+  var emailTest = false;
+  var passwordTest = false;
+  // Check for email
+  var email = $('#email').val();
+    if(email === "") {
+    $("p.email-error").text("Please enter your email.");
+    }
+    else {
+      emailTest = true;
+      $("p.email-error").text("");
+    };
+  
+  // Check for password
+  var password = $('#password').val();
+    var pattern = new RegExp()
+    if(password === "") {
+      $("p.password-error").text("Please enter your password.");
+    }
+     else if(password.length<8) {
+      $("p.password-error").text("Your password should be at least 8 characters.");
+    }
+    else {
+      passwordTest = true;
+      $("p.password-error").text("");
+    };
+
+  // Console log updated email and password  
+  console.log('email test = ' + emailTest);
+  console.log('password test = ' + passwordTest);
+  
+  // Pseudo-authentication
+  var redirect = function() {
+    $("p.success").text("Redirecting...");
+  };
+
+  var success = function() {
+    $(".loading-container").toggle();
+    $("p.success").text("Success!");
+    setTimeout(redirect,1000);
+    setTimeout(function(){
+      window.location.replace("dashboard.html")}, 1500);
+  };
+
+  if (emailTest && passwordTest) {
+    console.log('WOOHOO! Successful login.');
+    $(".loading-container").toggle();
+    setTimeout(success, 2500);
+  };
+
+  return false;
+});
+
+
+
+
+
